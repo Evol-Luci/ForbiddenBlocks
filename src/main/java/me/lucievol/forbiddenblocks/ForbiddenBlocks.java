287880d0cf301ab74a53c11070ab65e57d0d16fe
Luci_Evol <evolvoodoo@gmail.com>
2025-03-05 16:57:41 -0500

    Initial commit

---
+++ src/main/java/me/lucievol/forbiddenblocks/ForbiddenBlocks.java
@@ -0,0 +1,18 @@
+package me.lucievol.forbiddenblocks;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ForbiddenBlocks implements ModInitializer {
+	public static final String MOD_ID = "forbiddenblocks";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/me/lucievol/forbiddenblocks/ForbiddenBlocksClient.java
@@ -0,0 +1,10 @@
+package me.lucievol.forbiddenblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForbiddenBlocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // TODO Auto-generated method stub
+    }
+}
